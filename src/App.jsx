5bbce0e9ee0bdfc74f826cbd5c80898e25fa14da
xiong35 <2242787668@qq.com>
2021-02-01 21:25:07 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./App.scss";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="resume dark">
+        <h1 className="heading">熊逸朗的简历</h1>
+      </div>
+    );
+  }
+}
