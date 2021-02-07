68f72f555538a75bb236dc13f04d813832ff57a6
xiong35 <2242787668@qq.com>
2021-02-07 15:23:07 +0800

    small fix and add vue3

--- src/static/experiences.js
+++ src/static/experiences.js
@@ -3,12 +3,20 @@ const experiences = [
     name: "个人博客v1.0",
     from: "2020/4/7",
     to: "2020/5/12",
-    description: "用 vue + bootstrap + django 完成的第一版个人博客。",
+    description:
+      "用 vue + bootstrap + django 完成的第一版个人博客。",
     links: [
       { name: "部署地址", href: "http://www.xiong35.cn/blog1.0/" },
-      { name: "前端代码地址", href: "https://github.com/xiong35/MyBlog/tree/master/blog_vue" },
-      { name: "后端代码地址", href: "https://github.com/xiong35/MyBlog/tree/master/blog" },
-    ]
+      {
+        name: "前端代码地址",
+        href:
+          "https://github.com/xiong35/MyBlog/tree/master/blog_vue",
+      },
+      {
+        name: "后端代码地址",
+        href: "https://github.com/xiong35/MyBlog/tree/master/blog",
+      },
+    ],
   },
   {
     name: "个人博客v2.0",
@@ -16,56 +24,83 @@ const experiences = [
     to: "2020/6/20",
     description: "用 nuxt + vuetify + golang 完成的第二版博客。",
     links: [
-      { name: "部署地址", href: "http://www.xiong35.cn/blog2.0/articles" },
-      { name: "前端代码地址", href: "https://github.com/xiong35/nuxt-blog/" },
-      { name: "后端代码地址", href: "https://github.com/xiong35/go_blog_backend/" },
-    ]
+      {
+        name: "部署地址",
+        href: "http://www.xiong35.cn/blog2.0/articles",
+      },
+      {
+        name: "前端代码地址",
+        href: "https://github.com/xiong35/nuxt-blog/",
+      },
+      {
+        name: "后端代码地址",
+        href: "https://github.com/xiong35/go_blog_backend/",
+      },
+    ],
   },
   {
     name: "线上狼人杀",
     from: "2020/7/1",
     to: "2020/7/18",
-    description: "字节跳动夏令营完成的线上狼人杀任务，本人主要负责完成后端部分和文档编写。因为后端需要用到字节跳动的轻服务，而使用权限已被收回，所以现在没有成品网址。",
+    description:
+      "字节跳动夏令营完成的线上狼人杀任务，本人主要负责完成后端部分和文档编写。因为后端需要用到字节跳动的轻服务，而使用权限已被收回，所以现在没有成品网址。",
     links: [
-      { name: "前后端代码地址", href: "https://github.com/xiong35/techtrainingcamp-h-fe-3/" },
-    ]
+      {
+        name: "前后端代码地址",
+        href:
+          "https://github.com/xiong35/techtrainingcamp-h-fe-3/",
+      },
+    ],
   },
   {
     name: "Feelings",
     from: "2020/10/1",
     to: "2020/10/14",
-    description: "冰岩作坊实习期完成的使用 flutter 开发的音乐 app。",
+    description:
+      "冰岩作坊实习期完成的使用 flutter 开发的音乐 app。",
     links: [
-      { name: "apk 下载地址", href: "http://static.xiong35.cn/apk/feelings.apk" },
-      { name: "代码地址", href: "https://github.com/xiong35/Feelings/" },
-    ]
+      {
+        name: "apk 下载地址",
+        href: "http://static.xiong35.cn/apk/feelings.apk",
+      },
+      {
+        name: "代码地址",
+        href: "https://github.com/xiong35/Feelings/",
+      },
+    ],
   },
   {
     name: "iKnowGhost",
     from: "2020/10/19",
     to: "2020/10/27",
-    description: "一款用 vue 开发的 arg 解谜游戏，本人负责完成前端网页，代码地址不便公开。",
+    description:
+      "一款用 vue 开发的 arg 解谜游戏，本人负责完成前端网页，代码地址不便公开。",
     links: [
       { name: "部署地址", href: "https://ghost.hust.online/" },
-    ]
+    ],
   },
   {
     name: "WNM",
     from: "2020/11/4",
     to: "2020/12/24",
-    description: "Waste No Money，一款使用 flutter 开发的轻量级记账软件，代码地址不便公开。",
+    description:
+      "Waste No Money，一款使用 flutter 开发的轻量级记账软件，代码地址不便公开。",
     links: [
-      { name: "apk 下载地址", href: "http://static.xiong35.cn/apk/WNM.apk" },
-    ]
+      {
+        name: "apk 下载地址",
+        href: "http://static.xiong35.cn/apk/WNM.apk",
+      },
+    ],
   },
   {
     name: "Panda",
     from: "2021/1/16",
     to: "2021/2/1",
-    description: "一个用 react 进行前端开发，egg.js 编写后台逻辑的网站，用来记录和女朋友的点点滴滴。本人负责编写后台逻辑、和前端三个界面。",
+    description:
+      "一个用 react 进行前端开发，egg.js 编写后台逻辑的网站，用来记录和女朋友的点点滴滴。本人负责编写后台逻辑和前端的三个界面。",
     links: [
       { name: "部署地址", href: "http://cancan.xiong35.cn:99/" },
-    ]
+    ],
   },
   {
     name: "个人简历",
@@ -74,9 +109,12 @@ const experiences = [
     description: "用 react 编写的个人简历。",
     links: [
       { name: "部署地址", href: "http://resume.xiong35.cn" },
-      { name: "代码地址", href: "https://github.com/xiong35/resume" },
-    ]
+      {
+        name: "代码地址",
+        href: "https://github.com/xiong35/resume",
+      },
+    ],
   },
-]
+];
 
 export default experiences;
--- src/static/skills.js
+++ src/static/skills.js
@@ -1,28 +1,32 @@
 const skills = [
   {
-    name: "Vue",
-    level: 4
+    name: "Vue2",
+    level: 4,
   },
   {
     name: "React",
-    level: 4
+    level: 4,
   },
   {
     name: "Flutter",
-    level: 3
+    level: 3,
   },
   {
     name: "Node",
-    level: 3
+    level: 3,
+  },
+  {
+    name: "Vue3",
+    level: 2,
   },
   {
     name: "TypeScript",
-    level: 2
+    level: 2,
   },
   {
     name: "小程序",
-    level: 2
+    level: 2,
   },
-]
+];
 
 export default skills;
