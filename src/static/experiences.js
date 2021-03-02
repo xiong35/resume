0a49f2e6399702512efab840908055a8386abfab
xiong35 <2242787668@qq.com>
2021-03-02 09:54:15 +0800

    change exp description

--- src/static/experiences.js
+++ src/static/experiences.js
@@ -3,7 +3,8 @@ const experiences = [
     name: "个人博客v2.0",
     from: "2020/5/28",
     to: "2020/6/20",
-    description: "用 nuxt + vuetify + golang 完成的第二版博客。",
+    description:
+      "个人独立完成的第二版博客网站。使用 nuxt + vuetify + golang 开发。是学习前端的第二个项目，锻炼了 vue 的实战能力。现部署在服务器上，持续使用并维护。",
     links: [
       {
         name: "部署地址",
@@ -24,7 +25,7 @@ const experiences = [
     from: "2020/10/1",
     to: "2020/10/14",
     description:
-      "冰岩作坊实习期完成的使用 flutter 开发的音乐 app。",
+      "个人独立完成的音乐 app，使用 flutter 开发。凭此项目在冰岩作坊（知名高校互联网团队）实习期脱颖而出，加入其中。",
     links: [
       {
         name: "apk 下载地址",
@@ -41,7 +42,7 @@ const experiences = [
     from: "2021/1/16",
     to: "2021/2/1",
     description:
-      "一个用 react 进行前端开发，egg.js 编写后台逻辑的网站，用来记录和女朋友的点点滴滴。本人负责编写后台逻辑和前端的三个界面。",
+      "合作完成的生活记录网站。用 react 进行前端开发，egg.js 编写后台逻辑，用来记录和女朋友的点点滴滴。本人负责编写后台逻辑和前端的三个界面。现部署在服务器上，持续使用并维护。",
     links: [
       { name: "部署地址", href: "http://cancan.xiong35.cn:99/" },
     ],
