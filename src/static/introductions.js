0c7f36befbf95e371f7228d2e189db31554ebca3
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-10-17 15:17:26 +0800

    change info

--- src/static/introductions.js
+++ src/static/introductions.js
@@ -1,34 +1,41 @@
 const introductions = [
   {
-    name: "深入了解 JavaScript",
-    value: "仔细阅读过 ECMAScript 2021 标准规范。",
+    name: "有扎实的 JS/HTML/CSS 基础",
+    value: "读过多本相关书籍并有笔记产出",
   },
   {
     name: "熟悉前端框架",
     value:
-      "阅读过 Vue3 源码。熟练使用 Vue，React，了解二者底层原理，了解二者联系与区别，熟悉二者相关生态。",
+      "熟练使用 Vue 和 React，了解二者底层原理，了解二者联系与区别，熟悉二者相关生态",
   },
   {
-    name: "相比于大多数应届生，有较多实战经验",
-    value: "参与完成过十余个前端项目。",
+    name: "持续产出技术博客",
+    value: '博客部署于<a href="http://www.xiong35.cn">此地址</a>',
+  },
+  {
+    name: "有较多实战经验",
+    value:
+      '加入<a href="https://www.bingyan.net/">冰岩作坊团队</a>，参与完成过十余个前端项目',
   },
   {
     name: "有跨端开发能力",
     value:
-      "熟练使用 Node.js 进行后端开发。会使用 Flutter 和小程序进行开发。有人工智能相关开发经验。",
+      "熟练使用 Node.js 进行后端开发。会使用 Flutter 和小程序进行开发。有人工智能相关开发经验",
   },
   {
-    name: "具有抽象封装的能力",
-    value: "会在项目中抽离出通用组件、布局、工具函数、高阶函数等。",
+    name: "具有抽象封装的能力，了解设计模式",
+    value:
+      "会在项目中抽离出通用组件、布局、工具函数、高阶函数等，会在项目中使用设计模式",
   },
   {
     name: "熟练使用 TypeScript",
     value:
-      '目前正在挑战Type Challenge中的题目并将解答开源（<a href="https://github.com/xiong35/type-challenges-solutions">开源地址</a>）',
+      '目前正在挑战Type Challenge中的题目并将解答<a href="https://github.com/xiong35/type-challenges-solutions">开源</a>',
   },
   {
-    name: "掌握基本的算法和数据结构，了解设计模式",
-    value: "",
+    name: "掌握基本算法和数据结构",
+    value:
+      '除常见的数据结构和算法外，还出于兴趣了解并实现过<a href="https://github.com/xiong35/color-extraction">图片主色提取算法</a>和<a href="http://www.xiong35.cn/blog3.0/post/614d9878f166d62decd2c8af">多种机器学习算法</a>',
   },
 ];
 
